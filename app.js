9361e0ca8e791fb2e177d1899b033343ed336ba5
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-05 19:30:59 +0530

    node practic

---
+++ app.js
@@ -0,0 +1,17 @@
+//global
+console.log()
+
+setTimeout()
+clearTimeout()
+
+setInterval()
+clearInterval()
+
+var log = require('./logger');
+log.log('message');
+
+//path module
+//os module
+//file system module
+//Always use async methods
+//Event module
