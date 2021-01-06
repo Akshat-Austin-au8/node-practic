0f24fb13dba53f7d058e4bd6937cc0c3600fb200
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-06 23:26:48 +0530

    express practic-2

--- express-demo/app.js
+++ express-demo/app.js
@@ -9,7 +9,14 @@ app.get('/', (req, res) =>
 app.get('/api/courses', (req, res) =>
 {
     res.send([1, 2, 3]);
-})
+});
+
+app.get('/api/courses/:id ', (req, res) =>
+{
+    res.send(req.query);;
+});
+
+
 
 //PORT
 const port = process.env.PORT || 3000;
