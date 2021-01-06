13f00f1b60b7bf0f9175ab8f194495c7c2c45e50
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-06 23:33:12 +0530

    practic

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: "course1" },
+    { id: 2, name: "course2" },
+    { id: 3, name: "course3" },
+]
+
 app.get('/', (req, res) =>
 {
     res.send('hello worls');
@@ -8,12 +14,14 @@ app.get('/', (req, res) =>
 
 app.get('/api/courses', (req, res) =>
 {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id ', (req, res) =>
 {
-    res.send(req.query);;
+    const course = courses.find(c => c.id === parseInt(req.param.id));
+    if (!course) res.status(404).send('the course with this given id is undefined');
+    res.send(course);
 });
 
 
