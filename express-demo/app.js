1a9963c1c825a5e97a7d230c4860b92ea28a5fa5
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-06 23:18:58 +0530

    express -1

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,1 +1,16 @@
-require('express')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>
+{
+    res.send('hello worls');
+});
+
+app.get('/api/courses', (req, res) =>
+{
+    res.send([1, 2, 3]);
+})
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening to port ${port}...................`));
