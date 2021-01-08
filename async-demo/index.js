a96b677fd38a9e55a9329cf80f30348598615d49
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-08 23:24:39 +0530

    simple async js practic

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,16 @@
 console.log('before');
+getUser(1);
+console.log('after');
+
+//callback
+//promices
+//async/await
+
+function getUser(id)
+{
 setTimeout(() =>
 {
     console.log('Reading a user from a database......')
+    return { id: id, githubUSername: 'akshat' };
 }, 2000);
-console.log('after');
+}
