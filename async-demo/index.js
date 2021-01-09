b6f348fefe48c74f76c08e2a71e614f878d58dee
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-09 13:57:38 +0530

    callback practic

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,16 +1,19 @@
 console.log('before');
-getUser(1);
+getUser(1, (user) =>
+{
+    console.log('user', user)
+});
 console.log('after');
 
 //callback
 //promices
 //async/await
 
-function getUser(id)
+function getUser(id, callback)
 {
 setTimeout(() =>
-{
-    console.log('Reading a user from a database......')
-    return { id: id, githubUSername: 'akshat' };
-}, 2000);
-}
+    {
+        console.log('Reading a user from a database......')
+        callback ({ id: id, githubUsername: 'akshat' });
+    }, 2000);
+} 
