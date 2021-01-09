50d7042d00081ee7b1b66451322f99cdcacf07f3
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-09 14:10:52 +0530

    callback hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,8 +1,24 @@
+//Asychronous
+
 console.log('before');
 getUser(1, (user) =>
 {
-    console.log('user', user)
+    getRepositories(user.githubUsername, (repos) =>
+    {
+        getCommits(repo, (commits) =>
+        {
+            
+        });
+    });
 });
+console.log('after');
+
+//Synchronous
+
+console.log('before');
+const user = getUser(1);
+const repos = getRepositories(user.githubUsername);
+const commits = getCommits(repo[0]);
 console.log('after');
 
 //callback
@@ -15,5 +31,14 @@ setTimeout(() =>
     {
         console.log('Reading a user from a database......')
         callback ({ id: id, githubUsername: 'akshat' });
+    }, 2000);
+} 
+
+function getRepositories(username, callback)
+{
+setTimeout(() =>
+    {
+        console.log('Calling github apis......')
+        callback ([ 'user1', 'user2', 'user3' ]);
     }, 2000);
 } 
