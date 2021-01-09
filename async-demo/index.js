7500f6589ac10dfbded819d32191245fa2a4988e
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-09 14:19:50 +0530

    simplify callback hell to functions

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,43 +1,39 @@
-//Asychronous
-
 console.log('before');
-getUser(1, (user) =>
-{
-    getRepositories(user.githubUsername, (repos) =>
-    {
-        getCommits(repo, (commits) =>
-        {
-            
-        });
-    });
-});
+getUser(1, getRepositories);
 console.log('after');
+
+function getRepositories(user) {
+    getRepositories(user.githubUsername, getCommits);
+}
+
+function getCommits(repos) {
+    getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log(commits);
+}
 
 //Synchronous
-
-console.log('before');
-const user = getUser(1);
-const repos = getRepositories(user.githubUsername);
-const commits = getCommits(repo[0]);
-console.log('after');
+// console.log('before');
+// const user = getUser(1);
+// const repos = getRepositories(user.githubUsername);
+// const commits = getCommits(repo[0]);
+// console.log('after');
 
 //callback
 //promices
 //async/await
 
-function getUser(id, callback)
-{
-setTimeout(() =>
-    {
+function getUser(id, callback) {
+setTimeout(() => {
         console.log('Reading a user from a database......')
         callback ({ id: id, githubUsername: 'akshat' });
     }, 2000);
 } 
 
-function getRepositories(username, callback)
-{
-setTimeout(() =>
-    {
+function getRepositories(username, callback) {
+setTimeout(() => {
         console.log('Calling github apis......')
         callback ([ 'user1', 'user2', 'user3' ]);
     }, 2000);
