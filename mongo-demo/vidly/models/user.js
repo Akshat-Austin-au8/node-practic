9b83211a728a71a66f2fb6da646a48478614aa05
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-13 23:07:36 +0530

    update

--- mongo-demo/vidly/models/user.js
+++ mongo-demo/vidly/models/user.js
@@ -23,14 +23,14 @@ const User = mongoose.model('User',  new mongoose.Schema({
     }
 }));
 
-function validateUser(genre) {
+function validateUser(user) {
     const schema = {
-        name: Joi.string().min(3).required()
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required(),
     };
 
-    return Joi.validate(genre, schema);
+    return Joi.validate(user, schema);
 }
-
-exports.genreSchema = genreSchema;
-exports.Genre = User; 
+exports.User = User; 
 exports.validate = validateUser;
