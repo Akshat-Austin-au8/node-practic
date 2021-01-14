6497a3011307637f282c61c8fcf506370b15c40c
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-14 16:07:35 +0530

    auth-8

--- mongo-demo/vidly/models/user.js
+++ mongo-demo/vidly/models/user.js
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 5,
         maxlength: 1000
-    }
+    },
+    isAdmin: Boolean
 });
 
 userSchema.methods.generateAuthToken = function () {
--- mongo-demo/vidly/routes/users.js
+++ mongo-demo/vidly/routes/users.js
@@ -1,9 +1,16 @@
-const {User, validate} = require('../models/user');
+const { User, validate } = require('../models/user');
+const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+
+router.get('/:id', auth, async (req, res) =>
+{
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+});
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body); 
