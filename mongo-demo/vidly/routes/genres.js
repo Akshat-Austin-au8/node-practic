79fbb94a97a95c9257a3b82d3c6a741ed0fbf913
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-15 23:20:09 +0530

    improved_1

--- mongo-demo/vidly/routes/genres.js
+++ mongo-demo/vidly/routes/genres.js
@@ -1,4 +1,3 @@
-const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
@@ -6,13 +5,13 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', asyncMiddleware(async (req, res) =>
+router.get('/', async (req, res) =>
 {
     const genres = await Genre.find().sort('name');
     res.send(genres);
-}));
+});
 
-router.post('/', asyncMiddleware(async (req, res) =>
+router.post('/', async (req, res) =>
 {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
@@ -21,9 +20,9 @@ router.post('/', asyncMiddleware(async (req, res) =>
   genre = await genre.save();
   
   res.send(genre);
-}));
+});
 
-router.put('/:id', asyncMiddleware(async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -34,7 +33,7 @@ router.put('/:id', asyncMiddleware(async (req, res) => {
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   
   res.send(genre);
-}));
+});
 
 router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
